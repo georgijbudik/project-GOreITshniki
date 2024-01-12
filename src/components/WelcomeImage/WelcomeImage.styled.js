70dd4e4bc93ef066462bff9094ceecfdf496f6a5
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-12 22:13:06 +0200

    fixed welcome-image component

--- src/components/WelcomeImage/WelcomeImage.styled.js
+++ src/components/WelcomeImage/WelcomeImage.styled.js
@@ -1,12 +1,22 @@
 import styled from 'styled-components';
 
-export const StyledWelcomeImageWrapper = styled.div`
-  max-height: 100vh;
-  overflow: hidden;
-`;
-
-export const StyledWelcomeImage = styled.img`
-  height: auto;
+export const StyledWelcomeImagePicture = styled.picture`
+  position: absolute;
+  bottom: 0;
+  right: 0;
   display: block;
-  object-fit: cover;
+
+  max-width: 446px;
+
+  @media screen and (min-width: 768px) {
+    max-width: 670px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    max-width: 670px;
+  }
+
+  & > img {
+    display: block;
+  }
 `;
