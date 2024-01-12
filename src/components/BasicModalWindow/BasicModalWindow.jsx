5d4be972869f78c35acba7c84bfba05418b97e19
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-12 14:53:35 +0200

    added modal component in #modal-root

--- src/components/BasicModalWindow/BasicModalWindow.jsx
+++ src/components/BasicModalWindow/BasicModalWindow.jsx
@@ -1,5 +1,31 @@
-const BasicModalWindow = () => {
-  return <div>BasicModalWindow</div>;
+import { createPortal } from 'react-dom';
+
+import {
+  StyledModalOverlay,
+  StyledModal,
+  StyledModalContent,
+  StyledModalCloseButton,
+} from './BasicModalWindow.styled';
+
+const BasicModalWindow = ({ children }) => {
+  return createPortal(
+    <StyledModalOverlay>
+      <StyledModal>
+        <StyledModalCloseButton>
+          <svg width="22" height="22">
+            <use
+              xlinkHref={
+                process.env.PUBLIC_URL + '/images/sprite/sprite.svg#icon-cross'
+              }
+            />
+          </svg>
+        </StyledModalCloseButton>
+
+        <StyledModalContent>{children}</StyledModalContent>
+      </StyledModal>
+    </StyledModalOverlay>,
+    document.getElementById('modal-root')
+  );
 };
 
 export default BasicModalWindow;
---
+++ src/components/BasicModalWindow/BasicModalWindow.styled.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const StyledModalOverlay = styled.div`
+  z-index: 99999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(4, 4, 4, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledModal = styled.div`
+  border-radius: 12px;
+  border: 1px solid var(--border-color);
+  background: #10100f;
+  padding: 48px 24px;
+  color: var(--main-text-color);
+  position: relative;
+
+  @media screen and (min-width: 768px) {
+    padding: 48px 32px;
+  }
+`;
+
+export const StyledModalCloseButton = styled.button`
+  padding: 0;
+  line-height: 0;
+  background-color: transparent;
+  cursor: pointer;
+  border: none;
+
+  position: absolute;
+  top: 14px;
+  right: 14px;
+
+  stroke: var(--main-text-color);
+
+  transition: var(--transition);
+
+  &:hover {
+    stroke: var(--accent-color);
+    transform: rotate(90deg);
+  }
+
+  @media screen and (min-width: 768px) {
+    top: 16px;
+    right: 16px;
+  }
+`;
+
+export const StyledModalContent = styled.div``;
