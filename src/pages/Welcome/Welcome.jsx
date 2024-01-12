4464629ccdecb9bd8b089b8feac233e18ff9312e
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-12 13:54:09 +0200

    added button components

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { useState } from 'react';
+
+import Button from 'components/Button';
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const [disabled, setDisabled] = useState(true);
+
+  return (
+    <div>
+      <Button>Primary</Button>
+
+      <Button secondary>Secondary</Button>
+
+      <Button disabled={disabled}>Disabled</Button>
+
+      <Button
+        onClick={() => {
+          setDisabled(prev => !prev);
+        }}
+      >
+        onClick
+      </Button>
+    </div>
+  );
 };
 
 export default Welcome;
