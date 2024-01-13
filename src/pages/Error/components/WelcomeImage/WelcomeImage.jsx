146a1f25405a9f32be16b27c51cf2a49a4dd2b0f
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-13 18:41:12 +0200

    fixed error-page image sizes

--- src/pages/Error/components/WelcomeImage/WelcomeImage.jsx
+++ src/pages/Error/components/WelcomeImage/WelcomeImage.jsx
@@ -1,31 +1,7 @@
 import { StyledWelcomeImageWrapper } from './WelcomeImage.styled';
 
 const WelcomeImage = () => {
-  return <StyledWelcomeImageWrapper></StyledWelcomeImageWrapper>;
+  return <StyledWelcomeImageWrapper />;
 };
 
 export default WelcomeImage;
-
-// <StyledWelcomeImagePicture>
-//   {/* Mobile */}
-//   <source
-//     media="(max-width: 767px)"
-//     srcSet={`${publicUrl}/images/mobile/sm-welcome-image.png, ${publicUrl}/images/mobile/sm-welcome-image@2x.png 2x`}
-//   />
-//   {/* Tablet */}
-//   <source
-//     media="(min-width: 768px) and (max-width: 1439px)"
-//     srcSet={`${publicUrl}/images/tablet/md-welcome-image.png, ${publicUrl}/images/tablet/md-welcome-image@2x.png 2x`}
-//   />
-//   {/* Desktop */}
-//   <source
-//     media="(min-width: 1440px)"
-//     srcSet={`${publicUrl}/images/desktop/lg-welcome-image.png, ${publicUrl}/images/desktop/lg-welcome-image@2x.png 2x`}
-//   />
-//   {/* Default for all devices */}
-//   <img
-//     src={`${publicUrl}/images/desktop/lg-welcome-image.png`}
-//     srcSet={`${publicUrl}/images/desktop/lg-welcome-image@2x.png 2x`}
-//     alt="Woman doing squats"
-//   />
-// </StyledWelcomeImagePicture>;
--- src/pages/Error/components/WelcomeImage/WelcomeImage.styled.js
+++ src/pages/Error/components/WelcomeImage/WelcomeImage.styled.js
@@ -1,20 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledWelcomeImageWrapper = styled.div`
-  width: 298px;
+  width: 348px;
   overflow: hidden;
 
   background: linear-gradient(89deg, #040404 1.1%, rgba(4, 4, 4, 0) 70.79%);
-
   background-image: ${() =>
     `url(${process.env.PUBLIC_URL + '/images/mobile/sm-welcome-image.png'})`};
   background-repeat: no-repeat;
-  background-position: center center;
+  background-position: bottom 10% right 25%;
   background-size: 446px auto;
 
   @media screen and (max-width: 767px) and (min-resolution: 2dppx) {
-    width: 298px;
-
     background-image: ${() =>
       `url(${
         process.env.PUBLIC_URL + '/images/mobile/sm-welcome-image@2x.png'
@@ -22,21 +19,16 @@ export const StyledWelcomeImageWrapper = styled.div`
   }
 
   @media screen and (min-width: 768px) {
-    width: 437px;
+    width: 65.31%;
 
-    background-position: center right;
-    background-size: cover;
+    background-position: bottom right;
+    background-size: contain;
 
     background-image: ${() =>
       `url(${process.env.PUBLIC_URL + '/images/tablet/md-welcome-image.png'})`};
   }
 
   @media screen and (min-width: 768px) and (min-resolution: 2dppx) {
-    width: 437px;
-
-    background-position: center right;
-    background-size: cover;
-
     background-image: ${() => {
       return `url(${
         process.env.PUBLIC_URL + '/images/tablet/md-welcome-image@2x.png'
@@ -45,7 +37,7 @@ export const StyledWelcomeImageWrapper = styled.div`
   }
 
   @media screen and (min-width: 1440px) {
-    width: 100%;
+    width: 53.54%;
 
     background-size: contain;
     background-position: center right;
@@ -57,34 +49,9 @@ export const StyledWelcomeImageWrapper = styled.div`
   }
 
   @media screen and (min-width: 1440px) and (min-resolution: 2dppx) {
-    width: 100%;
-
-    background-size: contain;
-    background-position: center right;
-
     background-image: ${() =>
       `url(${
         process.env.PUBLIC_URL + '/images/desktop/lg-welcome-image@2x.png'
       })`};
-  }
-`;
-
-export const StyledWelcomeImagePicture = styled.picture`
-  display: block;
-
-  max-width: 446px;
-  object-fit: contain;
-  object-position: center;
-
-  @media screen and (min-width: 768px) {
-    max-width: 670px;
-  }
-
-  @media screen and (min-width: 1440px) {
-    max-width: 670px;
-  }
-
-  & > img {
-    display: block;
   }
 `;
