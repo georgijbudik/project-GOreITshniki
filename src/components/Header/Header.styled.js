f0e6bdc8eea3b2956debe4cc40e6aaef35dd7a7a
Oleksandr <bajmutov@gmail.com>
2024-01-14 16:00:50 +0200

    logout/thunk
    Добавив кнопку логаут, написав thunk

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const UserSvgLogout = styled.svg`
+  margin-left: 8px;
+`;
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm';
+import { Heading, Info } from './SignInForm/SignInForm.styled';
 
 const SignIn = () => {
   return (
     <>
-      <h2>Sign In</h2>
-      <p>Welcome! Please enter your credentials to login to the platform:</p>
+      <Heading>Sign In</Heading>
+      <Info>
+        Welcome! Please enter your credentials to login to the platform:
+      </Info>
       <SignInForm />
+      <Info>
+        Don’t have an account? <Link to="/signup">Sign Up</Link>
+      </Info>
     </>
   );
 };
