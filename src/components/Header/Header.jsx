cabf5571a5b873796fbc026f4f2d71903ded4b24
Oleksandr <bajmutov@gmail.com>
2024-01-15 19:45:08 +0200

    auth
    подружив фронт з беком, працює логін, сайнап, логаут, рефреш, підключив персіст

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,52 @@
 import Button from 'components/Button';
 import { UserSvgLogout } from './Header.styled';
+import { selectIsLoggedin } from '../../redux/auth/authSelectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut } from '../../redux/auth/authOperations';
 
 export function Header() {
+  const isLoggedIn = useSelector(selectIsLoggedin);
+  const dispatch = useDispatch();
+  // const email = useSelector(selectUserEmail);
+
+  const handleLogOut = () => {
+    console.log('fuck logout');
+    dispatch(logOut());
+    // toast(`We will miss you, ${email}`, {
+    //   duration: 2000,
+    //   position: 'top-right',
+    //   icon: '🤠',
+    //   style: {
+    //     borderRadius: '10px',
+    //     background: '#333',
+    //     color: '#fff',
+    //   },
+    // });
+  };
+
   return (
     <div>
-      <Button type="button" secondary paddingY={12} paddingX={12}>
-        Logout
-        <UserSvgLogout width="20" height="20" stroke="#E6533C">
-          <use
-            xlinkHref={
-              process.env.PUBLIC_URL + '/images/sprite/sprite.svg#icon-log-out'
-            }
-          ></use>
-        </UserSvgLogout>
-      </Button>
+      {isLoggedIn && (
+        <Button
+          type="button"
+          secondary
+          paddingY={12}
+          paddingX={12}
+          onClick={handleLogOut}
+        >
+          Logout
+          <UserSvgLogout width="20" height="20" stroke="#E6533C">
+            <use
+              xlinkHref={
+                process.env.PUBLIC_URL +
+                '/images/sprite/sprite.svg#icon-log-out'
+              }
+            ></use>
+          </UserSvgLogout>
+        </Button>
+      )}
     </div>
   );
 }
+
+// {isLoggedIn ? <UserMenu /> : <AuthNav />}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { globalReducer } from './global/globalSlice';
 import { authReducer } from './auth/authSlice';
 
+const authPersistConfig = {
+  key: 'auth',
+  version: 1,
+  storage,
+  whitelist: ['token'],
+};
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
     globalReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
