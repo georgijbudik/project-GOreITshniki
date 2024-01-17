edca3f02c815325c4b999a95079bc453e9eef4f0
Olena <shapovalev@gmail.com>
2024-01-17 16:12:05 +0100

    backgroundImage

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 // import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
+import TitlePage from 'components/TitlePage';
+import Loader from 'components/Loader';
 import ProductsFilters from './ProductsFilters';
 import ProductsList from './ProductsList';
-import { ProductsSection } from './Products.styled';
-import TitlePage from 'components/TitlePage';
-import { useSelector } from 'react-redux';
 import AddProductForm from './AddProductForm';
-import { selectIsLoading, selectIsProductSuccesAdded, selectProductToAdd } from '../../redux/products/productSlice';
 import AddProductSuccess from './AddProductSuccess';
-import Loader from 'components/Loader';
+import {
+  selectIsLoading,
+  selectIsProductSuccesAdded,
+  selectProductToAdd,
+} from '../../redux/products/productSlice';
 // import { fetchCategories, fetchProducts} from '../../redux/products/productOperations';
+
+import { StyledWrapper, StyledSection } from './Products.styled';
 
 const Products = () => {
   // const dispatch = useDispatch();
@@ -25,17 +31,19 @@ const Products = () => {
   const isProductToAdd = useSelector(selectProductToAdd);
   const isProductSuccesAdded = useSelector(selectIsProductSuccesAdded);
   const isLoading = useSelector(selectIsLoading);
-  
+
   return (
-    <main> 
+    <main>
       {isLoading && <Loader />}
-      {isProductSuccesAdded && <AddProductSuccess/>}
-      {isProductToAdd && <AddProductForm/>}
-      <ProductsSection>
-        <TitlePage>Products</TitlePage>
+      {isProductSuccesAdded && <AddProductSuccess />}
+      {isProductToAdd && <AddProductForm />}
+      <StyledWrapper>
+        <StyledSection>
+          <TitlePage>Products</TitlePage>
         <ProductsFilters />
         <ProductsList />
-      </ProductsSection>
+        </StyledSection>
+      </StyledWrapper>
     </main>
   );
 };
--- src/pages/Products/Products.styled.js
+++ src/pages/Products/Products.styled.js
@@ -1,12 +1,36 @@
 import styled from 'styled-components';
 
-export const ProductsSection = styled.section`
+export const StyledWrapper = styled.div`
+  @media screen and (min-width: 1440px) {
+    background: linear-gradient(89deg, #040404 1.1%, rgba(4, 4, 4, 0) 70.79%);
+    background-image: ${() =>
+      `url(${
+        process.env.PUBLIC_URL + '/images/desktop/lg-products-image.jpg'
+      })`};
+    background-repeat: no-repeat;
+    background-position: top right;
+    background-size: auto 100%;
+  }
+
+  @media screen and (min-width: 1440px) and (min-resolution: 2dppx) {
+    background-image: ${() =>
+      `url(${
+        process.env.PUBLIC_URL + '/images/desktop/lg-products-image@2x.jpg'
+      })`};
+  }
+`;
+
+export const StyledSection = styled.section`
+  box-sizing: border-box;
   padding: 40px 0 80px 0;
   color: var(--main-text-color);
 
   @media screen and (min-width: 768px) {
+    padding: 72px 0 48px 0;
   }
 
   @media screen and (min-width: 1440px) {
+    padding: 0 96px;
+    padding: 72px 0 81px 0;
   }
 `;
