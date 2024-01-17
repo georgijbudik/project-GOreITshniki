9ed27da95dcf49993de3374c58501889a8daf132
AndriiMishch <anderito02@gmail.com>
2024-01-17 21:03:31 +0100

    Update index.js

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -19,3 +19,88 @@ export const ButtonContainer = styled.div`
   display: flex;
   gap: 14px;
 `;
+
+export const BodyLine = styled.svg`
+position: absolute;
+left: -9px;
+top: 6px;
+`;
+
+export const BodyElementSpan = styled.span`
+position: relative;
+`
+export const TrackerVideoPlayEl = styled.div`
+width: 30px;
+height: 30px;
+background-color:#EF8964;
+border-radius:50%;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const TrackerVideoIcon = styled.svg`
+padding-left:2px;
+`
+
+export const TrackerVideoStyled = styled.div`
+color: #efede8;
+`
+
+export const TrackerVideoContainer = styled.div`
+width: 146px;
+height: 66px;
+flex-shrink: 0;
+border-radius: 12px;
+background: #303030;
+justify-content:center;
+display:flex;
+align-items: center;
+`
+export const TrackerVideoText = styled.div`
+color: rgba(239, 237, 232, 0.65);
+font-size: 12px;
+line-height: 16px;
+`
+export const TrackerVideoTextContainer = styled.div`
+padding-left:8px;
+`
+
+export const TrackerCaloriesStyled = styled.div`
+color: #EFEDE8;
+
+font-family: Roboto;
+font-size: 24px;
+font-style: normal;
+font-weight: 700;
+line-height: 24px; /* 100% */
+letter-spacing: -1px;
+text-transform: uppercase;
+`
+export const TrackerCaloriesContainer = styled.div`
+width: 146px;
+height: 66px;
+border-radius: 12px;
+background: #EF8964;
+`
+export const TrackerCaloriesIconEl = styled.div`
+background-color:#EFA082;
+border-radius:50%;
+width: 20px;
+height: 20px;
+justify-content:center;
+display: flex;
+align-items: center;
+`
+export const TrackerCaloriesIcon = styled.svg`
+fill: #EFEDE8;
+`
+
+export const TrackerCaloriesText = styled.span`
+color: rgba(239, 237, 232, 0.65);
+font-size: 12px;
+font-weight: 700;
+line-height: 16px; /* 133.333% */
+letter-spacing: 0;
+text-transform: none;
+padding-left:8px;
+`
