119457a777b4821e533c343fbaa5f13412a33176
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2024-01-19 03:11:44 +0100

    exerc Modal,exerc Item-fix

--- src/pages/Exercises/SectionTemplate/SectionTemplate.styled.js
+++ src/pages/Exercises/SectionTemplate/SectionTemplate.styled.js
@@ -6,19 +6,19 @@ export const Section = styled.section`
   height: 100vh;
 
   @media screen and (min-width: 1440px) {
-    background-repeat: no-repeat;
-    background-position: right bottom;
+    /* background-repeat: no-repeat;
+    background-position: right bottom; */
     /* background-position-y: calc(100% + 50px); */
-    background-size: contain;
-    background-image: ${() => {
+    /* background-size: contain; */
+    /* background-image: ${() => {
       return `url('${process.env.PUBLIC_URL}/images/desktop/lg-welcome-image.jpg')`;
-    }};
+    }}; */
 
     @media only screen and (min-resolution: 192dpi),
       only screen and (min-resolution: 2dppx) {
-      background-image: ${() => {
+      /* background-image: ${() => {
         return `url('${process.env.PUBLIC_URL}/images/desktop/lg-welcome-image@2x.jpg')`;
-      }};
+      }}; */
     }
   }
 `;
