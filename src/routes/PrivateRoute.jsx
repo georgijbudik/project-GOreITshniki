c7ed455d98bf081838921ad21c0fe8447e39ac03
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-01-23 13:34:39 +0200

    fixed navigation

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,18 @@
 import { useSelector } from 'react-redux';
-import { selectIsLoggedin } from '../redux/auth/authSelectors';
+import { selectIsLoggedin, selectUserBlood } from '../redux/auth/authSelectors';
 
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children, navigateTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedin);
+  const userInfo = useSelector(selectUserBlood);
+
+  if (!userInfo) {
+    return <Navigate to="/profile" replace />;
+  }
 
   if (!isLoggedIn) {
-    return <Navigate to={navigateTo} />;
+    return <Navigate to={navigateTo} replace />;
   }
 
   return children;
